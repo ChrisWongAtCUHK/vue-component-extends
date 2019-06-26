94f921a5a2e80d1bcc9200efc2c9591b6a7640b1
chriswongatcuhk <wc38.wong@gmail.com>
2019-06-26 21:50:02 +0800

    AdminListPage通过extends继承了ListPageAbstract的模板、样式和其JS代码

---
+++ src/components/ButtonClick.vue
@@ -0,0 +1,16 @@
+<!-- 展示操作按钮和执行操作事件 -->
+<template>
+<div>
+  <el-button size="small" plain type="primary" @click.stop="click">
+    {{ label }}
+  </el-button>
+</div>
+</template>
+<script type="text/javascript">
+  export default {
+    props: ['click', 'label', 'opt'],
+    mounted () {
+      console.log('ButtonClick mounted')
+    }
+  }
+</script>
---
+++ src/components/Title.vue
@@ -0,0 +1,9 @@
+<!-- 用于显示页面的标题（之后我们用它测试下继承于ListPageAbstract.vue的组件如何重写css的问题） -->
+<template>
+  <div class="title">{{ title }}</div>
+</template>
+<script type="text/javascript">
+export default {
+  props: ['title']
+}
+</script>
